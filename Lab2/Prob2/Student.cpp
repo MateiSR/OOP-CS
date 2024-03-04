6f24bd812fc1be1499da18b752fd36d726fe7581
MateiSR <MateiSR@proton.me>
2024-03-04 19:53:52 +0200

    Fix lab2 pr2

--- Lab2/Prob2/Student.cpp
+++ Lab2/Prob2/Student.cpp
@@ -1,29 +1,29 @@
 #include "Student.h"
 
 Student::Student() {
-  Student::grades.math = Student::grades.english = Student::grades.history = 0;
+  this->grades.math = this->grades.english = this->grades.history = 0;
 }
 
 void Student::setName(char *name, int length) {
   for (int i = 0; i < length; i++)
-    Student::name[i] = *(name + i);
-  Student::name[length] = '\0';
+    this->name[i] = *(name + i);
+  this->name[length] = '\0';
 }
 
-char *Student::getName() { return Student::name; }
+char *Student::getName() { return this->name; }
 
-void Student::setEnglishGrade(float grade) { Student::grades.english = grade; }
+void Student::setEnglishGrade(float grade) { this->grades.english = grade; }
 
-void Student::setMathGrade(float grade) { Student::grades.math = grade; }
+void Student::setMathGrade(float grade) { this->grades.math = grade; }
 
-void Student::setHistoryGrade(float grade) { Student::grades.history = grade; }
+void Student::setHistoryGrade(float grade) { this->grades.history = grade; }
 
-float Student::getEnglishGrade() { return Student::grades.english; };
-float Student::getMathGrade() { return Student::grades.math; };
-float Student::getHistoryGrade() { return Student::grades.history; };
+float Student::getEnglishGrade() { return this->grades.english; };
+float Student::getMathGrade() { return this->grades.math; };
+float Student::getHistoryGrade() { return this->grades.history; };
 
 float Student::getAvgGrade() {
-  return (Student::grades.math + Student::grades.history +
-          Student::grades.english) /
+  return (this->grades.math + this->grades.history +
+          this->grades.english) /
          3.0;
 }
