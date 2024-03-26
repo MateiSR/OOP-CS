266051ff29a128f5c9ed1a99382a545786ff10f5
MateiSR <MateiSR@proton.me>
2024-03-26 14:13:56 +0200

    Update Lab5 (TBD: --(prefix, postfix) & +=)

--- Lab5/Prob1/Number.cpp
+++ Lab5/Prob1/Number.cpp
@@ -1,27 +1,7 @@
 #include "Number.h"
 #include <cstdio>
+#include <cstdlib>
 #include <string.h>
-#include <math.h>
-
-/*
-void Number::reallocHeap() {
-  char *newVal;
-  if (this->value == nullptr) {
-    newVal = new char[this->charSize + 1];
-    for (int i = 0; i < this->charSize; i++)
-      newVal[i] = '\0';
-  } else {
-    newVal = new char[this->charSize * 2 + 1];
-    for (int i = this->charSize; i < this->charSize * 2; i++)
-      newVal[i] = '\0';
-    for (int i = 0; i < this->charSize; i++)
-      newVal[i] = this->value[i];
-    this->charSize *= 2;
-    delete[] this->value;
-  }
-  this->value = newVal;
-}
-*/
 
 int Number::GetBase() { return this->base; }
 int Number::GetDigitsCount() { return this->length; }
@@ -31,6 +11,25 @@ Number::Number(const char *value, int base) {
   this->value = new char[len + 1];
   this->length = len;
   strcpy(this->value, value);
+}
+
+Number::Number(int num) : base(10) {
+  int copyNum = num, _len = 0;
+  do {
+    _len++;
+    copyNum /= 10;
+  } while (copyNum != 0);
+  char *newVal = new char[_len + 1];
+  sprintf(newVal, "%d", num);
+  this->value = newVal;
+  this->length = _len;
+}
+
+Number::Number(const char *value) : base(10) {
+  this->length = strlen(value);
+  char *newVal = new char[this->length + 1];
+  strcpy(newVal, value);
+  this->value = newVal;
 }
 
 Number::~Number() { delete[] this->value; }
@@ -59,35 +58,44 @@ int charToInt(char ch) {
 }
 
 char intToChar(int d) {
-  if (d >= 0 && d <= 9) return d + '0';
-  else if (d >= 10 && d <= 15) return 'A' + d - 10;
+  if (d >= 0 && d <= 9)
+    return d + '0';
+  else if (d >= 10 && d <= 15)
+    return 'A' + d - 10;
   return 'X';
 }
 
 void Number::SwitchBase(int newBase) {
+  if (this->base == newBase)
+    return;
   // assume base is between 2 and 16
   unsigned long long int valueB10 = 0;
   for (int i = 0; i < this->length; i++) {
     valueB10 = valueB10 * this->base + charToInt(this->value[i]);
   }
-  // printf("valueb10: %d\n", valueB10);
-  char *buf = new char[1024];
+  // printf("original value, base: %d\n", this->base);
+  // this->Print();
+  // printf("valueb10: %llu\n", valueB10);
+  char *buf = new char[this->MAXBUFSIZE];
   int bufLength = 0;
   while (valueB10) {
     int digit = valueB10 % newBase;
+    // printf("curr digit: %d\n", digit);
     buf[bufLength++] = intToChar(digit);
     valueB10 /= newBase;
   }
+  // printf("newbase: %d\n", newBase);
   buf[bufLength] = '\0';
+  // printf("value of buf:%s\n", buf);
   // printf("buf: %s\n", buf);
-  // 
   // reverse buf
   int start = 0, end = bufLength - 1;
   while (start < end) {
     char aux = buf[start];
     buf[start] = buf[end];
     buf[end] = aux;
-    start++; end--;
+    start++;
+    end--;
   }
 
   // printf("bufnew: %s\n", buf);
@@ -95,13 +103,137 @@ void Number::SwitchBase(int newBase) {
   char *newVal = new char[bufLength + 1];
   for (int i = 0; i <= bufLength; i++)
     newVal[i] = buf[i];
-  
 
-  delete[] buf; // delete the buffer
-  delete[] this->value; // delete old value 
+  delete[] buf;         // delete the buffer
+  delete[] this->value; // delete old value
   this->base = newBase;
   this->length = bufLength;
   this->value = newVal;
 }
 
+Number &Number::operator=(const Number &num) {
+  if (this != &num) {
+    base = num.base;
+    if (this->value != nullptr)
+      delete[] this->value;
+    char *newVal = new char[num.length + 1];
+    strcpy(newVal, num.value);
+    this->value = newVal;
+    this->length = num.length;
+  }
+  return *this;
+}
 
+char &Number::operator[](int index) { return this->value[index]; }
+
+Number operator+(const Number &num1, const Number &num2) {
+  Number num1c(num1.value, num1.base);
+  Number num2c(num2.value, num2.base);
+  unsigned int resultBase = num1.base > num2.base ? num1.base : num2.base;
+  num1c.SwitchBase(10);
+  num2c.SwitchBase(10);
+  // printf("num1c:\n");
+  // num1c.Print();
+  // printf("num2c:\n");
+  // num2c.Print();
+  unsigned int numRes = atoi(num1c.value) + atoi(num2c.value);
+  int _len = 0;
+  unsigned int resCopy = numRes;
+  do {
+    _len++;
+    resCopy /= 10;
+  } while (resCopy != 0);
+  char *additionRes = new char[_len + 1];
+  sprintf(additionRes, "%d", numRes);
+  Number res(additionRes, 10);
+  // printf("---");
+  // res.Print();
+  res.SwitchBase(resultBase);
+  return res;
+};
+
+Number operator-(const Number &num1, const Number &num2) {
+  Number num1c(num1.value, num1.base);
+  Number num2c(num2.value, num2.base);
+  unsigned int resultBase = num1.base > num2.base ? num1.base : num2.base;
+  num1c.SwitchBase(10);
+  num2c.SwitchBase(10);
+  // printf("num1c:\n");
+  // num1c.Print();
+  // printf("num2c:\n");
+  // num2c.Print();
+  unsigned int numRes = atoi(num1c.value) - atoi(num2c.value);
+  int _len = 0;
+  unsigned int resCopy = numRes;
+  do {
+    _len++;
+    resCopy /= 10;
+  } while (resCopy != 0);
+  char *subtractionRes = new char[_len + 1];
+  sprintf(subtractionRes, "%d", numRes);
+  Number res(subtractionRes, 10);
+  // printf("---");
+  // res.Print();
+  res.SwitchBase(resultBase);
+  return res;
+};
+
+bool Number::operator>(const Number &num) {
+  Number thisB10 = *this;
+  thisB10.SwitchBase(10);
+  Number cmpB10 = num;
+  cmpB10.SwitchBase(10);
+  return atoi(thisB10.value) > atoi(cmpB10.value);
+}
+
+Number &Number::operator+=(const Number &num) {
+  const int resultBase = this->base > num.base ? this->base : num.base;
+  Number numCopy = num;
+  this->SwitchBase(10);
+  numCopy.SwitchBase(10);
+  unsigned int numRes = atoi(numCopy.value) + atoi(this->value);
+  int _len = 0;
+  unsigned int resCopy = numRes;
+  do {
+    _len++;
+    resCopy /= 10;
+  } while (resCopy != 0);
+  char *additionRes = new char[_len + 1];
+  if (this->value != nullptr)
+    delete[] this->value;
+  this->value = additionRes;
+  this->length = _len;
+  this->base = 10;
+  this->SwitchBase(resultBase);
+  return *this;
+}
+
+Number &Number::operator--() { // prefix
+  char *oldVal = this->value;
+  if (this->value != nullptr)
+    delete[] this->value;
+  if (this->length <= 1) {
+    char *newVal = new char[1];
+    newVal[0] = 0;
+    this->value = newVal;
+    this->length = 0;
+  } else {
+    char *newVal = new char[this->length];
+    for (int i = 1; i < this->length; i++)
+      newVal[i] = oldVal[i - 1];
+    newVal[--this->length] = 0;
+  }
+  return *this;
+}
+
+Number &Number::operator--(int) { // postfix
+  char *oldVal = this->value;
+  if (this->value != nullptr)
+    delete[] this->value;
+  char *newVal = new char[this->length];
+  for (int i = 0; i < this->length - 1; i++)
+    newVal[i] = oldVal[i];
+  newVal[--this->length] = 0;
+  this->value = newVal;
+  return *this;
+}
--- Lab5/Prob1/Number.h
+++ Lab5/Prob1/Number.h
@@ -8,11 +8,14 @@ class Number {
   char *value = nullptr;
   int base = 10;
   int length = 0;
+  const int MAXBUFSIZE = 1024;
   friend int charToInt(char ch);
   friend char intToChar(int d);
 
 public:
   Number(const char *value, int base); // where base is between 2 and 16
+  Number(int num);
+  Number(const char *value);
   ~Number();
 
   // add operators and copy/move constructor
@@ -20,23 +23,22 @@ public:
   Number(Number &&other);
 
   // operators
-  friend Number operator+(const Number& num1, const Number& num2);
-  friend Number operator-(const Number& num1, const Number& num2);
-  char& operator[](int index);
-  bool operator>(const Number& num);
-  bool operator<(const Number& num);
-  bool operator>=(const Number& num);
-  bool operator<=(const Number& num);
-  bool operator==(const Number& num);
-  bool operator!=(const Number& num);
-  Number& operator+=(const Number& num);
-  Number& operator--();
-  Number& operator--(int);
-  // 
+  friend Number operator+(const Number &num1, const Number &num2);
+  friend Number operator-(const Number &num1, const Number &num2);
+  Number &operator=(const Number &num);
+  char &operator[](int index);
+  bool operator>(const Number &num);
+  bool operator<(const Number &num);
+  bool operator>=(const Number &num);
+  bool operator<=(const Number &num);
+  bool operator==(const Number &num);
+  bool operator!=(const Number &num);
+  Number &operator+=(const Number &num);
+  Number &operator--();    // Prefix
+  Number &operator--(int); // Postfix
+  //
   void SwitchBase(int newBase);
   void Print();
   int GetDigitsCount(); // returns the number of digits for the current number
   int GetBase();        // returns the current base
-  // void reallocHeap();
-  // void addChar(char ch);
 };
